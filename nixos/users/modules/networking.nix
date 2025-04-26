cc130c0594d5100244075929b5e5b30cf4b3b93f
Glitch752 <xxGlitch752xx@gmail.com>
2025-04-25 20:35:25 -0500

    Adjust avahi config

--- nixos/users/modules/networking.nix
+++ nixos/users/modules/networking.nix
@@ -3,7 +3,17 @@
   networking.networkmanager.enable = true;
 
   # Avahi is an mDNS/DNS-SD (Zeroconf) implementation.
-  services.avahi.enable = true;
+  services.avahi = {
+    nssmdns = true;
+    enable = true;
+    ipv4 = true;
+    ipv6 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+    };
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];

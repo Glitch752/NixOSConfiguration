5b7e81cd76fe7b05aff720850a491d77dc59a228
Glitch752 <xxGlitch752xx@gmail.com>
2025-05-22 00:21:37 -0500

    Switch to FHS VSCode

--- home-manager/users/modules/editors.nix
+++ home-manager/users/modules/editors.nix
@@ -1,21 +1,13 @@
 { inputs, lib, config, pkgs, ... }: {
   # https://nixos.wiki/wiki/Visual_Studio_Code
   programs.vscode = {
-    package =
-      (pkgs.vscode.override
-        {
-          # https://wiki.archlinux.org/title/Wayland#Electron
-          commandLineArgs = [
-            "--ozone-platform-hint=auto"
-            "--enable-features=UseOzonePlatform"
-            "--ozone-platform=wayland"
-            # make it use GTK_IM_MODULE if it runs with Gtk4, so fcitx5 can work with it.
-            # (only supported by chromium/chrome at this time, not electron)
-            "--gtk-version=4"
-            # make it use text-input-v1, which works for kwin 5.27 and weston
-            "--enable-wayland-ime"
-          ];
-        });
+    package = pkgs.vscode.fhsWithPackages (ps: with ps; [
+      # For rust
+      rustup
+      zlib
+      openssl.dev
+      pkg-config
+    ]);
 
     enable = true;
 

de01daaf37f71da1eac6a1ac84c093f41533866d
Glitch752 <xxGlitch752xx@gmail.com>
2025-05-25 14:10:37 -0500

    Fix VSCode desktop entry

--- home-manager/users/modules/editors.nix
+++ home-manager/users/modules/editors.nix
@@ -31,4 +31,29 @@
       }
     ];
   };
+
+  # Override the vscode desktop entry to not include %F because it breaks when using FHS code
+  xdg.desktopEntries.code = {
+    categories = [ "Utility" "TextEditor" "Development" "IDE" ];
+    comment = "Code Editing. Redefined.";
+    exec = "code --enable-features=UseOzonePlatform,WaylandWindowDecorations --ozone-platform-hint=auto";
+    genericName = "Text Editor";
+    icon = "vscode";
+    name = "Visual Studio Code";
+    startupNotify = true;
+    settings = {
+      Keywords = "vscode";
+      StartupWMClass = "Code";
+      Version = "1.4";
+    };
+    type = "Application";
+
+    actions = {
+      "new-empty-window" = {
+        name = "New Empty Window";
+        exec = "code --new-window --enable-features=UseOzonePlatform,WaylandWindowDecorations --ozone-platform-hint=auto";
+        icon = "vscode";
+      };
+    };
+  };
 }
